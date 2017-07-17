a9c1e3d24ee6f0da3a2b0aa1f2d981a683bd1f44
Premkumar Chandrashegaran <premkumar@skillslab.co>
2017-07-17 22:59:18 +0800

    Initial commit. Android Signature Example

---
+++ app/src/main/java/co/skillslab/signatureviewsample/SignatureActivity.java
@@ -0,0 +1,42 @@
+package co.skillslab.signatureviewsample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+public class SignatureActivity extends AppCompatActivity {
+
+    private DrawingView drawView;
+    private ImageButton currPaint;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signature);
+
+        drawView = (DrawingView)findViewById(R.id.drawing);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currPaint = (ImageButton)paintLayout.getChildAt(0);
+        currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+
+
+    }
+
+    public void paintClicked(View view){
+        //use chosen color
+        if(view!=currPaint){
+//update color
+            ImageButton imgView = (ImageButton)view;
+            String color = view.getTag().toString();
+
+            drawView.setColor(color);
+
+            imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+            currPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
+            currPaint=(ImageButton)view;
+        }
+    }
+
+}
